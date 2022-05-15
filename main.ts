319fc3a0aebcf55af6c5416f5350c6fd6db3df2c
tommyzhang12345678 <102849446+tommyzhang12345678@users.noreply.github.com>
2022-05-15 11:39:00 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+led.plot(2, 2)
+let _ = 0
+let _2 = 0
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.A)) {
+        _ += 1
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        _2 += 1
+    }
 })
